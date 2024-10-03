6e81e972a7393479b6b91a457930ba49314fea02
Sagar Vishwakarma <sv472921@gmail.com>
2024-10-03 11:08:14 +0530

    commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -18,6 +18,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 
+
 function App() {
 
 
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
+
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy:{
-      '/user':'http://localhost:6000'
+      '/user':process.env.SERVER_URL || ' http://localhost:6000'
     },
   },
   plugins: [react()],
